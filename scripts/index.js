d5f8f823d452aec9a04effa3a3cbe11007b9f3b9
Mayur199627 <mayur27asati@gmail.com>
2023-01-28 12:42:42 +0530

    authentication

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+// All Variable Declaration //
+const userEmail = document.getElementById('email');
+const userPassword = document.getElementById('password');
+const signinBtn = document.getElementById('login-form'); 
+
+// Function for Reset Input //
+
+function reset(){
+    userEmail.value = "";
+    userPassword.value = "";
+}
+
+// Function for check user
+
+function checkUser(event){
+    event.preventDefault();
+    let options = {
+    body:JSON.stringify({  
+        email : userEmail.value,
+        pass : userPassword.value,
+    }),
+    headers : {
+        "content-type" : "application/json"
+    },
+    method:'POST'
+    };
+
+fetch("https://aircampushack.onrender.com/forms/login",options)
+.then((resolve)=>{
+    alert("User Logged In Successfully")
+    window.location.href = "main.html";
+    return resolve.json() 
+
+}).catch((error)=>{
+    alert(error);
+})
+    reset();
+}
+
+// Event Listener on Signin
+
+signinBtn.addEventListener('submit',checkUser)
---
+++ scripts/signup.js
@@ -0,0 +1,54 @@
+// All Variables Declaration //
+
+const username = document.getElementById('name');
+const userEmail = document.getElementById('email');
+const userPass = document.getElementById('password');
+const userConfirmPass = document.getElementById('confirm-password');
+const SignUpBtn = document.getElementById('submit')
+
+
+//Function for Reset //
+function reset(){
+    username.value = "";
+    userEmail.value = "";
+    userPass.value = "";
+    userConfirmPass.value = "";
+}
+
+// Function for Add User //
+function addNewUser(event){
+    event.preventDefault();
+    if(userPass.value !== userConfirmPass.value){
+        alert("Password does not match")
+    }
+    else{
+        let id = Date.now();
+
+        let options = {
+            body:JSON.stringify({  
+            email : userEmail.value,
+            pass : userPass.value,
+            }),
+        headers : {
+            "content-type" : "application/json"
+        },
+        method:'POST'
+        };
+
+        fetch("https://aircampushack.onrender.com/forms/signin",options)
+        .then((resolve)=>{
+            alert("User Register Successfully")
+            window.location.href = "main.html";
+            return resolve.json() 
+
+        }).catch((error)=>{
+            alert(error);
+        })
+    }
+    
+    reset()
+    
+}
+
+// Event Listener for SignUp
+   SignUpBtn.addEventListener('click',addNewUser)
