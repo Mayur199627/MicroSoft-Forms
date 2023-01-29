201d77f9e81e1b7651385b457380cbc24f9ed324
Mayur199627 <mayur27asati@gmail.com>
2023-01-29 22:39:08 +0530

    changes

--- scripts/form.js
+++ scripts/form.js
@@ -12,7 +12,9 @@
         const questionInput = document.querySelector('.save-question input')
         const plusBtn = document.querySelector(".right-main")
         const leftContainer = document.querySelector(".all-questions")
-        
+        const sendBtn = document.getElementById('send-btn')
+        const modal = document.querySelector('.modal-container')
+        const crossIcon = document.querySelector('.cross')
         //Function For Edit Name Of Document //
 
         function changeName(){
@@ -35,23 +37,31 @@
         // Function for change color of star //
 
         function changeColor(){
-            if(star.style.backgroundColor == ""){
-                star.style.backgroundColor = "yellow";
+            if(star.classList.contains('stared')){
+                star.removeAttribute('class','stared')
             }else{
-                star.style.backgroundColor = "";
+                star.setAttribute('class','stared')
             }
         }
 
         // Function for display question tab
 
         function showQuestion(){
+            responseText.removeAttribute('class','underlined');
+            if(!questionText.classList.contains("underlined")){
+                questionText.setAttribute('class','underlined')
+            }
             questionTab.style.display = 'flex';
             responseTab.style.display = 'none';
         }
 
-        // Function for display question tab
+        // Function for display Response tab
 
         function showResponse(){
+            questionText.removeAttribute('class','underlined');
+            if(!responseText.classList.contains('underlined')){
+                responseText.setAttribute('class','underlined')
+            }
             questionTab.style.display = 'none';
             responseTab.style.display = 'flex';
         }
@@ -94,18 +104,29 @@
         // Function for Adding Question //
 
         function addQuestions(){
+            let id = Date.now()
             leftContainer.insertAdjacentHTML("beforeend", `<div class="questions-list">
             <div class="question">
                 <div class="question-text" style="font-size: 1.2rem;">Question</div>
-                <div class="save-question"><input type="text" class="text-feild"></div>
+                <div class="save-question"><input type="text" class="text-feild" name="${id}"></div>
                 <div class="question-option">
-                    <input type="radio" name="" value=""> option1<br>
-                    <input type="radio" name="" value=""> option2
+                    <input type="radio" name="${id}" value=""> True<br>
+                    <input type="radio" name="${id}" value=""> False
                 </div>
             </div>
         </div>`)
         }
 
+        // Function for Show Modal //
+        function showModal(){
+            modal.style.display = 'flex'
+        }
+
+        // Function for hide Modal //
+        function hideModal(){
+            modal.style.display = 'none'
+        }
+        
 
         // All Event Listner
         docName.addEventListener('click',changeName)
@@ -117,3 +138,30 @@
         questionInput.addEventListener('blur',saveQuestion)
         document.addEventListener('click',saveAll)
         plusBtn.addEventListener('click',addQuestions)
+        sendBtn.addEventListener('click',showModal)
+        crossIcon.addEventListener('click',hideModal)
+        
+        document.addEventListener('click',(e)=>{
+            if(e.target.classList.contains('question-text')){
+                let targetValue = e.target;
+                let targetInput = e.target.nextElementSibling.firstElementChild;
+                targetValue.style.display = 'none'
+                targetInput.style.display = "flex"
+            }
+
+
+            if(e.target.classList.contains('text-feild')){
+                let ele = e.target;
+                let eleParent = e.target.parentElement;
+                let inputValue = ele.value;
+                let inputBox = e.target.parentElement.previousElementSibling;
+                if(inputValue !== ""){
+                    inputBox.innerHTML = inputValue;
+                }
+                ele.addEventListener('blur',()=>{
+                    eleParent.style.display = "none";
+                    inputBox.style.display = "flex";
+
+                })
+            }
+        })
